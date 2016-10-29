85944ea2dde6bfc91d81b52264818a244ed90e8a
mrkoolaid <mrkoolaid@protonmail.com>
2016-10-28 21:40:08 -0700

    Signed-off-by: mrkoolaid <mrkoolaid@protonmail.com>

---
+++ KraftLib/Converters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KraftLib
+{
+    public class Converters
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public string GetStrFromBytes(byte[] bytes)
+        {
+            return Encoding.ASCII.GetString(bytes);
+        }
+    }
+}
