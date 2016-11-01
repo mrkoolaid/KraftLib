f70a18abf5c6f75f84721e181bb304b43daeb586
mrkoolaid <mrkoolaid@protonmail.com>
2016-11-01 00:16:37 -0700

    - added aes encrypt/decrypt by mrmutt - added aes encrypt by codeh4ck - added a codeh4ck random string method
    Signed-off-by: mrkoolaid <mrkoolaid@protonmail.com>

--- KraftLib/Encryption.cs
+++ KraftLib/Encryption.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,102 @@ namespace KraftLib
 {
     class Encryption
     {
-        //
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="Pass"></param>
+        /// <author>Codeh4ck</author>
+        /// <returns></returns>
+        public static byte[] Codeh4ckAESEncrypt(byte[] input, string Pass)
+        {
+            RijndaelManaged AES = new RijndaelManaged();
+            byte[] hash = new byte[32];
+            byte[] temp = new MD5CryptoServiceProvider().ComputeHash(Encoding.ASCII.GetBytes(Pass));
+            Array.Copy(temp, 0, hash, 0, 16);
+            Array.Copy(temp, 0, hash, 15, 16);
+            AES.Key = hash;
+            AES.Mode = CipherMode.ECB;
+            ICryptoTransform DESEncrypter = AES.CreateEncryptor();
+            return DESEncrypter.TransformFinalBlock(input, 0, input.Length);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <author>mrmutt</author>
+        /// <param name="decrypted"></param>
+        /// <param name="key2"></param>
+        /// <returns></returns>
+        public static byte[] MrMuttAESDecrypt(byte[] decrypted, byte[] key2)
+        {
+            byte[] decryptedBytes = null;
+
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(key2, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(decrypted, 0, decrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+            return decryptedBytes;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <author>mrmutt</author>
+        /// <param name="encrypt"></param>
+        /// <param name="key2"></param>
+        /// <returns></returns>
+        public static byte[] MrMuttAESEncrypt(byte[] encrypt, byte[] key2)
+        {
+            byte[] encryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(key2, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(encrypt, 0, encrypt.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+
+            return encryptedBytes;
+        }
     }
 }
--- KraftLib/Generators.cs
+++ KraftLib/Generators.cs
@@ -9,6 +9,23 @@ namespace KraftLib
 {
     public class Generators
     {
-        //
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="length"></param>
+        /// <author>Codeh4ck</author>
+        /// <returns></returns>
+        private string Codeh4ckRandString(int length)
+        {
+            string pool = "abcdefghijklmnopqrstuvwxyz";
+            pool += pool.ToUpper();
+            string tmp = "";
+            Random R = new Random();
+            for (int x = 0; x < length; x++)
+            {
+                tmp += pool[R.Next(0, pool.Length)].ToString();
+            }
+            return tmp;
+        }
     }
 }
